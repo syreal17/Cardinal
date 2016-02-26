ef3cfe0a2a37b9281620ac47ceb88d163166aad8
syreal17 <syreal14@yahoo.com>
2016-02-26 10:14:47 -0800

    Added jaccard comp for bloom filters

---
+++ similarity/bloom-jaccard/bloom_jaccard_ind.py
@@ -0,0 +1,28 @@
+#-----------------------------------------------------------------------------
+# A Three-Pronged Approach to Exploring the Limits of Static Malware Analyses:
+# Callsite Parameter Cardinality (CPC) Counting: bloom_jaccard_ind.py
+#
+# Compares two or more Bloom filters by a Jaccard index and prints report
+#
+# Luke Jones (luke.t.jones.814@gmail.com)
+#
+#-----------------------------------------------------------------------------
+from __future__ import print_function
+import sys
+
+from pybloom import BloomFilter
+
+def jaccard_ind(filename_1, filename_2):
+    with open(filename_1, 'rb') as f_1:
+        with open(filename_2, 'rb') as f_2:
+            b_1 = BloomFilter.fromfile(f_1)
+            b_2 = BloomFilter.fromfile(f_2)
+            b_inter = b_1.intersection(b_2)
+            b_union = b_1.union(b_2)
+            #bitarry.count.....
+
+
+if __name__ == '__main__':
+    for filename_1 in sys.argv[1:]:
+        for filename_2 in sys.argv[1:]:
+            to_bloom(filename)
