89c5500b557d415628a152820978cf2856d0b26f
syreal17 <syreal14@yahoo.com>
2016-03-10 16:19:38 -0800

    Created script to create dat files for gnuplot

---
+++ tests/create_dats.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+get_result () {
+sample=$5
+comp=$2
+meth=$3
+iso=$4
+		grep "$sample,$comp,$meth,$iso" big_four.report | cut -d " " -f 2 | awk '{printf("%s",$0);}' >> $1 
+printf " " >> $1
+}
+
+samples=""
+for var in "$@"
+do
+		samples="$samples $var"
+done
+
+get_dat () {
+file=$1
+comp=$2
+meth=$3
+printf "" > $file
+isos="O0-O1 O0-O2 O0-O3 O1-O2 O1-O3 O2-O3"
+for iso in $isos
+do
+	printf "$iso " >> $file
+	for sample in $samples
+	do
+		get_result $file $comp $meth $iso $sample
+	done 
+	printf "\n" >> $file
+done
+}
+
+get_dat iso.clang.cpc.bloom.dat clang cpc-bloom
