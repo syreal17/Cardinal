38df918dd8dc8aee21238e3610b7ec12e950d402
syreal17 <syreal14@yahoo.com>
2016-07-13 14:02:09 -0700

    Changed bitcode filename to just .llvm to reflect new script

--- tests/windows/create_dicts.sh
+++ tests/windows/create_dicts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find ../../corpora/ -wholename */$1.*.elf -exec idaw64.exe -A -S"../../dev/feature_extraction/cpc/ida_cpc_extract.py -d" {} \;
-find ../../corpora/lin_bitcode_corpus/ -wholename */$1.*.llvm.o -exec sh -c '../../dev/ground_truth/build/Debug/cpc-tool.exe {} > {}.cpc.gdict' \;
+find ../../corpora/lin_bitcode_corpus/ -wholename */$1.llvm -exec sh -c '../../dev/ground_truth/build/Debug/cpc-tool.exe {} > {}.cpc.gdict' \;
