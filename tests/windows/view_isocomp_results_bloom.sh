9be50659ae1593dab5590541e077093951df6cb3
ltjones18 <luke.jones.ctr@usafa.edu>
2016-08-25 10:55:58 -0600

    Adding error checking to view_* scripts

--- tests/windows/view_isocomp_results_bloom.sh
+++ tests/windows/view_isocomp_results_bloom.sh
@@ -22,8 +22,67 @@ then
 	exit 1
 fi
 
-n=$RANDOM
-find ../../corpora/ -wholename "*/$1.*$2*.$3.feature.bloom" -exec python ../../dev/similarity/bloom-jaccard/bloom_jaccard_ind.py {} + > report.$1.$2.$3.txt
+#one liner with no error checking
+#find ../../corpora/ -wholename "*/$1.*$2*.$3.feature.bloom" -exec python ../../dev/similarity/bloom-jaccard/bloom_jaccard_ind.py {} + > report.$1.$2.$3.txt
+
+o=""
+s=`find ../../corpora/ -wholename "*/$1.*$2.o0.*.$3.feature.bloom"`
+count=`printf "%s" $s | wc -w`
+if [ $count -ne 1 ]; then
+	if [ $count -eq 0 ]; then
+		printf "o0 not found for $1,$2,$3\n"
+		exit 1
+	else
+		printf "Multiple o0's found for $1,$2,$3\n"
+		exit 1
+	fi
+fi
+o+=$s
+o+=" "
+
+s=`find ../../corpora/ -wholename "*/$1.*$2.o1.*.$3.feature.bloom"`
+count=`printf "%s" $s | wc -w`
+if [ $count -ne 1 ]; then
+	if [ $count -eq 0 ]; then
+		printf "o1 not found for $1,$2,$3\n"
+		exit 1
+	else
+		printf "Multiple o1's found for $1,$2,$3\n"
+		exit 1
+	fi
+fi
+o+=$s
+o+=" "
+
+s=`find ../../corpora/ -wholename "*/$1.*$2.o2.*.$3.feature.bloom"`
+count=`printf "%s" $s | wc -w`
+if [ $count -ne 1 ]; then
+	if [ $count -eq 0 ]; then
+		printf "o2 not found for $1,$2,$3\n"
+		exit 1
+	else
+		printf "Multiple o2's found for $1,$2,$3\n"
+		exit 1
+	fi
+fi
+o+=$s
+o+=" "
+
+s=`find ../../corpora/ -wholename "*/$1.*$2.o3.*.$3.feature.bloom"`
+count=`printf "%s" $s | wc -w`
+if [ $count -ne 1 ]; then
+	if [ $count -eq 0 ]; then
+		printf "o3 not found for $1,$2,$3\n"
+		exit 1
+	else
+		printf "Multiple o3's found for $1,$2,$3\n"
+		exit 1
+	fi
+fi
+o+=$s
+o+=" "
+
+python ../../dev/similarity/bloom-jaccard/bloom_jaccard_ind.py $o > report.$1.$2.$3.txt
 
 printf "isocomp, $1-$2-$3:\n" 
 
